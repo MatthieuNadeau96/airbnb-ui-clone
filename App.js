6602f9364a9c00d9b744e2e79ba095733ecf2e14
Matthieu <matthieunadeau96@gmail.com>
2019-07-14 13:21:26 -0400

    created bottom tab navigator & screens

--- App.js
+++ App.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import { createBottomTabNavigator } from 'react-navigation'
+
+import Explore from './screens/Explore'
+import Inbox from './screens/Inbox'
+import Saved from './screens/Saved'
+import Trips from './screens/Trips'
+
+class function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
 }
+
+export default createBottomTabNavigator({
+  Explore: {
+    screen: Explore
+  },
+  Saved: {
+    screen: Inbox
+  },
+  Trips: {
+    screen: Saved
+  },
+  Inbox: {
+    screen: Trips
+  }
+})
 
 const styles = StyleSheet.create({
   container: {
---
+++ screens/Inbox.js
@@ -0,0 +1,24 @@
+/* @flow */
+
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+} from 'react-native';
+
+export default class Inbox extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>I'm the Inbox component</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
