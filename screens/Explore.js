a74d0b5945d8d68cd0e23a4e4a6b12221054b7e3
Matthieu <matthieunadeau96@gmail.com>
2019-07-15 17:35:10 -0400

    fixed ScrollView bug & added Airbnb Luxe section to the Explore screen

--- screens/Explore.js
+++ screens/Explore.js
@@ -11,11 +11,14 @@ import {
   StatusBar,
   ScrollView,
   Image,
+  Dimensions,
 } from 'react-native';
 
 import Icon from 'react-native-vector-icons/Ionicons'
 
 import Category from './components/Explore/Category'
+
+const { height, width } = Dimensions.get('window')
 
 export default class Explore extends Component {
 
@@ -29,7 +32,7 @@ export default class Explore extends Component {
   render() {
     return (
       <SafeAreaView>
-        <View style={{flex: 1}}>
+        <View>
           <View style={{height: this.startHeaderHeight, backgroundColor: 'white', borderBottomWidth: 1, borderBottomColor: '#dddddd'}}>
             <View
               style={{
@@ -52,7 +55,7 @@ export default class Explore extends Component {
               />
             </View>
           </View>
-          <View scrollEventThrottle={16}>
+          <ScrollView scrollEventThrottle={16}>
             <View style={{flex: 1, backgroundColor: 'white', paddingTop: 20}}>
               <Text style={{fontSize: 24, fontWeight: '700', paddingHorizontal: 20}}>
                 What can we help you find, Matthieu?
@@ -62,10 +65,25 @@ export default class Explore extends Component {
                   <Category imageUri={require('../assets/home.jpg')} name="Stays" desc="Homes & more"/>
                   <Category imageUri={require('../assets/activites.jpg')} name="Experiences" desc="Hosted activites"/>
                   <Category imageUri={require('../assets/trips.jpg')} name="Adventures" desc="Hosted trips"/>
+                  <Category imageUri={require('../assets/restaurant.jpg')} name="Restaurants" desc="Top-rated eats"/>
                 </ScrollView>
               </View>
+              <View style={{marginTop: 40, paddingHorizontal: 20}}>
+                <Text style={{fontSize: 24, fontWeight: '700'}}>
+                  Introducing Airbnb Luxe
+                </Text>
+                <Text style={{marginTop: 10, fontWeight: '100'}}>
+                  Extraordinary homes with five-star everything
+                </Text>
+                <View style={{width: width-40, height: 200, marginTop: 20}}>
+                  <Image
+                    source={require('../assets/deluxe.jpg')}
+                    style={{flex: 1, height: null, width: null, resizeMode: 'cover', borderRadius: 5, borderWidth: 1, borderColor:'#dddddd'}}
+                  />
+                </View>
+              </View>
             </View>
-          </View>
+          </ScrollView>
         </View>
       </SafeAreaView>
     );
