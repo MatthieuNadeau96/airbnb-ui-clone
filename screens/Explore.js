4858ea39d0fe97b515261f9c22adc354d551ca09
Matthieu <matthieunadeau96@gmail.com>
2019-07-15 16:41:26 -0400

    Added ScrollView and started implementation of an explore card

--- screens/Explore.js
+++ screens/Explore.js
@@ -8,7 +8,9 @@ import {
   SafeAreaView,
   TextInput,
   Platform,
-  StatusBar
+  StatusBar,
+  ScrollView,
+  Image,
 } from 'react-native';
 
 import Icon from 'react-native-vector-icons/Ionicons'
@@ -36,16 +38,38 @@ export default class Explore extends Component {
                 shadowOffset: {width: 0, height: 0},
                 shadowColor: 'black',
                 shadowOpacity: 0.2,
-                elevation: 1,
+                elevation: 4,
                 marginTop: Platform.OS == 'android' ? 30 : null,
               }}>
               <Icon name="md-search" size={20} style={{marginRight: 10}}/>
               <TextInput
                 underlineColorAndroid="transparent"
-                placeholder="Try new Delhi"
+                placeholder="Try 'Tokyo'"
                 placeholderTextColor="grey"
                 style={{flex: 1, fontWeight: '700', backgroundColor: 'white'}}
               />
+            </View>
+          </View>
+          <View scrollEventThrottle={16}>
+            <View style={{flex: 1, backgroundColor: 'white', paddingTop: 20}}>
+              <Text style={{fontSize: 24, fontWeight: '700', paddingHorizontal: 20}}>
+                What can we help you find, Matt?
+              </Text>
+              <View style={{height: 130, marginTop: 20}}>
+                <ScrollView>
+                  <View style={{height: 130, width: 130, marginLeft: 20, borderWidth: 0.8, borderColor: '#dddddd'}}>
+                    <View style={{flex: 2}}>
+                      <Image
+                        source={require('../assets/home.jpg')}
+                        style={{flex: 1, width: null, height: null, resizeMode: 'cover'}}
+                      />
+                    </View>
+                    <View style={{flex: 1, paddingLeft: 10, paddingTop: 10}}>
+                      <Text>Home</Text>
+                    </View>
+                  </View>
+                </ScrollView>
+              </View>
             </View>
           </View>
         </View>
