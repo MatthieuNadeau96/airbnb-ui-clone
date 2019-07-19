d94bf2b35bb9e02efc28502f3188d80497c41c1e
Matthieu <matthieunadeau96@gmail.com>
2019-07-19 13:39:16 -0400

    changed styling of the category cards

--- screens/Explore.js
+++ screens/Explore.js
@@ -91,7 +91,7 @@ export default class Explore extends Component {
               <Text style={{fontSize: 24, fontWeight: '700', paddingHorizontal: 20}}>
                 What can we help you find, Matthieu?
               </Text>
-              <View style={{height: 130, marginTop: 20}}>
+              <View style={{height: 160, marginTop: 20}}>
                 <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
                   <Category imageUri={require('../assets/home.jpg')} name="Stays" desc="Homes & more"/>
                   <Category imageUri={require('../assets/activites.jpg')} name="Experiences" desc="Hosted activites"/>
--- screens/components/Explore/Category.js
+++ screens/components/Explore/Category.js
@@ -9,14 +9,14 @@ import {
 export default class Category extends Component {
   render() {
     return (
-      <View style={{height: 130, width: 130, marginLeft: 20, borderWidth: 0.8, borderColor: '#dddddd', borderRadius: 3}}>
+      <View style={{height: 160, width: 130, marginLeft: 20, borderWidth: 0.8, borderColor: '#dddddd', borderRadius: 3}}>
         <View style={{flex: 2}}>
           <Image
             source={this.props.imageUri}
             style={{flex: 1, width: null, height: null, resizeMode: 'cover'}}
           />
         </View>
-        <View style={{flex: 1, paddingLeft: 10, paddingTop: 10}}>
+        <View style={{flex: 1, paddingLeft: 10, paddingTop: 10, paddingBottom: 10}}>
           <Text>{this.props.name}</Text>
           <Text style={{fontSize: 10, color: '#666666'}}>{this.props.desc}</Text>
         </View>
